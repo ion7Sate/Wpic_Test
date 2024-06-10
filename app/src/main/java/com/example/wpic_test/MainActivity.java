50068d75f22205dd46dbf149905b9d56f4714302
ion7Sate <saptesate31@gmail.com>
2024-06-10 13:07:37 +0200

    Added statusbar and implemented spacing between frames

--- app/src/main/java/com/example/wpic_test/MainActivity.java
+++ app/src/main/java/com/example/wpic_test/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.wpic_test;
 
-import static com.example.wpic_test.R.*;
-
-import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
-
-import android.widget.ImageButton;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView timeTextView;
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
 
-    private TextView textMessage;
+    private Runnable updateTimeTask = new Runnable() {
+        @Override
+        public void run() {
+            String currentTime = timeFormat.format(new Date());
+            timeTextView.setText(currentTime);
+            handler.postDelayed(this, 60000);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,33 +36,23 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ImageButton buttonAdd = findViewById(R.id.button_add);
-        ImageButton buttonSettings = findViewById(R.id.button_settings);
-        textMessage = findViewById(R.id.text_message);
+        timeTextView = findViewById(R.id.tv_time);
+        handler.post(updateTimeTask);
 
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onAddButtonClick();
-            }
-        });
 
-        buttonSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onSettingsButtonClick();
-            }
-        });
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+
+
     }
 
-    private void onAddButtonClick() {
-        textMessage.setVisibility(View.VISIBLE);
-        textMessage.setText("Add Button is pressed");
-    }
-
-    private void onSettingsButtonClick() {
-        textMessage.setVisibility(View.VISIBLE);
-        textMessage.setText("Settings Button is pressed");
-    }
 
 }
